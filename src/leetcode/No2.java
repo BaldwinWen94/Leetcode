b0756ef621a1664e0501f4d0693d0cf00109b0d4
BaldwinWen <rwen@paypal.com>
2019-12-09 15:20:29 +0800

    add No2 & No3

---
+++ src/leetcode/No2.java
@@ -0,0 +1,41 @@
+/*
+ * Created By Baldwin Wen
+ */
+
+package leetcode;
+
+public class No2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode rootNode = new ListNode(-1);
+        ListNode loopNode = rootNode;
+        int flag = 0;
+
+        while (l1 != null || l2 != null || flag != 0) {
+            if (l1 != null) {
+                flag += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                flag += l2.val;
+                l2 = l2.next;
+            }
+
+            loopNode.next = new ListNode(flag % 10);
+            flag = flag / 10;
+
+            loopNode = loopNode.next;
+        }
+
+        return rootNode.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/leetcode/No3.java
@@ -0,0 +1,26 @@
+/*
+ * Created By Baldwin Wen
+ */
+
+package leetcode;
+
+import java.util.HashMap;
+
+public class No3 {
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int j = 0, i = 0; j < s.length(); j++) {
+            Character tmp = s.charAt(j);
+            if (map.containsKey(tmp)) {
+                i = Math.max(map.get(tmp), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(tmp, j + 1);
+        }
+        return ans;
+    }
+
+    // HashSet is another solution
+}
