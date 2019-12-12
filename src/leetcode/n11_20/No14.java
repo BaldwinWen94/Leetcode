8a3f1686ed9847edcc5a690b412a5698eaffd874
Baldwin Wen <13641975489@163.com>
2019-12-12 14:59:52 +0800

    add No14

---
+++ src/leetcode/n11_20/No14.java
@@ -0,0 +1,30 @@
+/*
+ * Created By Baldwin Wen
+ */
+
+package leetcode.n11_20;
+
+public class No14 {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        String first = strs[0];
+
+        int pos = 1;
+        while(pos <= first.length()){
+            String tmp = first.substring(0, pos);
+            boolean flag = true;
+            for (String str : strs) {
+                if (!str.startsWith(tmp)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag){
+                break;
+            }else{
+                pos++;
+            }
+        }
+        return first.substring(0, pos - 1);
+    }
+}
